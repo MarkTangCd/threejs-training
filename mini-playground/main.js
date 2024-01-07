a0cc0536ce57f3fb3d65c889c3bed420929bc57b
0xMark <jayzhoucd@gmail.com>
2024-01-07 20:38:19 +0800

    feat: filed middle ring

--- mini-playground/main.js
+++ mini-playground/main.js
@@ -16,16 +16,26 @@ function init() {
   renderer.setClearColor(new THREE.Color(0x000000));
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  const geometry = new THREE.PlaneGeometry(20, 10);
+  // field
+  const filedGeometry = new THREE.PlaneGeometry(20, 10);
   const material = new THREE.MeshBasicMaterial({
     color: new THREE.Color(0x6c9b42),
     side: THREE.DoubleSide,
   });
-  const mesh = new THREE.Mesh(geometry, material);
+  const mesh = new THREE.Mesh(filedGeometry, material);
   mesh.rotation.x = 2;
   scene.add(mesh);
 
-  const edges = new THREE.EdgesGeometry(geometry);
+  const ringGeometry = new THREE.RingGeometry(0.9, 1, 32);
+  const ringMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  const ringMesh = new THREE.Mesh(ringGeometry, ringMaterial);
+  // ringMesh.position.y = 1;
+  ringMesh.position.set(0, 0, 2);
+  ringMesh.rotation.x = 1;
+  scene.add(ringMesh);
+
+  // border
+  const edges = new THREE.EdgesGeometry(filedGeometry);
   const line = new THREE.LineSegments(
     edges,
     new THREE.LineBasicMaterial({ color: 0xffffff, linewidth: 100 })
